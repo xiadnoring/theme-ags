c3a1eb3d5273cdfba52cfa1767f3ef90fcac68f4
Xiadnoring <timurtuty132@gmail.com>
2025-03-27 17:05:33 +0500

    UPD | Dialog Widget #2

--- widget/FileChooser.tsx
+++ widget/FileChooser.tsx
@@ -7,8 +7,9 @@ import path from "../modules/path";
 import { DialogWindow } from "./Dialog";
 
 const MAX_COLS = 6;
+const ROWS_BY_STEP = 20;
 
-async function generate_file_element (filepath: string, file: Gio.FileInfo): Promise<Gtk.Widget> {
+async function generate_file_element (filepath: string, file: Gio.FileInfo, current_path: Variable<string>): Promise<Gtk.Widget> {
     const isfile = await fs.is_file (filepath);
     let icon: string;
     if (isfile) {
@@ -18,9 +19,7 @@ async function generate_file_element (filepath: string, file: Gio.FileInfo): Pro
         icon = 'folder-colored';
     }
 
-    return <button className="btn btn-square" onClick={() => {
-
-    }}>
+    return <button className="btn btn-square" onClicked={() => current_path.set(filepath)}>
         <box vertical={true} className="m-2">
             <box hexpand={true} halign={Gtk.Align.CENTER} valign={Gtk.Align.CENTER}>
                 <icon icon={icon} className="font-4"></icon>
@@ -32,25 +31,35 @@ async function generate_file_element (filepath: string, file: Gio.FileInfo): Pro
     </button>;
 }
 
-async function grab_files_and_fill (files_view: Gtk.Grid, filepath: string): Promise<{ rows: number, isfolder: boolean }> {
+async function grab_files_and_fill (files_view: Gtk.Grid, filepath: string, current_path: Variable<string>, prev_rows: number = 0): Promise<{ rows: number, isfolder: boolean }> {
     let rows = 0;
     let cols = 0;
+    let prev_cols = prev_rows * MAX_COLS;
     let isfolder = await fs.is_directory (filepath);
 
     if (isfolder) {
-        const files = await fs.list_dir (filepath);
-        files_view.insert_row(rows++ * MAX_COLS);
-        for (const file of files) {
-            
+        let files = await fs.list_dir (filepath);
+        let flg = true;
+        for await (const file of files) {
+            if (prev_cols) {
+                /** skip */
+                prev_cols--;
+                continue;
+            }
+            else if (flg) {
+                files_view.insert_row((rows++ + prev_rows) * MAX_COLS);
+                flg = false;
+            }
+
             const p = path.join(filepath, file.get_name());
-            files_view.insert_column((rows - 1) * MAX_COLS + cols++);
-            files_view.attach (await generate_file_element(p, file) ,cols-1,rows-1, 1,1);
+            files_view.insert_column((rows + prev_rows - 1) * MAX_COLS + cols++);
+            files_view.attach (await generate_file_element(p, file, current_path) ,cols-1,rows+prev_rows-1, 1,1);
             if (cols >= MAX_COLS) {
                 cols=0;
-                if (rows >= 20) {
+                if (rows >= ROWS_BY_STEP) {
                     break;
                 }
-                files_view.insert_row((rows++) * MAX_COLS);
+                files_view.insert_row((rows++ + prev_rows) * MAX_COLS);
                 
             }
         }
@@ -64,6 +73,8 @@ async function grab_files_and_fill (files_view: Gtk.Grid, filepath: string): Pro
 
 export function FileChooserWindow (gdkmonitor: Gdk.Monitor, props: { multiple?: boolean }, callback: (files: string[]) => void) {
     DialogWindow (gdkmonitor, (close) => {
+        const history: string[] = [];
+        let ready = Variable(false);
         let current_path = Variable('');
         
         let rows = 0;
@@ -71,40 +82,93 @@ export function FileChooserWindow (gdkmonitor: Gdk.Monitor, props: { multiple?: 
         const bd = new before_delete();
         const files_view = new GtkGrid ({ });
 
-        bd.add(current_path.subscribe((path: string) => {
+        async function grap_next_rows (path: string) {
+            ready.set(false);
+
+            try {
+                // FILL
+                await grab_files_and_fill (files_view, path, current_path, rows).then (data => {
+                    if (data.isfolder) {
+                        rows += data.rows;
+                    }
+                });
+            }
+            catch (e) {
+                if (e instanceof Error) {
+                    Log.exception (e, "grab_files_and_fill(...) failed. FileChooser");
+                }
+            }
+
+            ready.set(true);
+        }
+
+        function forward_path (path: string) {
             if (!path) {
                 return;
             }
 
-            for (let i = 0; i < rows; i++) {
-                files_view.remove_row(i);
+            while (true) {
+                if (files_view.get_child_at(0,0)) {
+                    files_view.remove_row(0);
+                }
+                else {
+                    break
+                }
             }
 
             rows = 0;
 
-            // FILL
-            grab_files_and_fill (files_view, path).then (data => {
-                rows = data.rows;
-            });
-        }));
+            grap_next_rows(path)
+                .finally (() => {
+                    history.push(path); });  
+        }
+
+        bd.add(current_path.subscribe(forward_path));
 
         current_path.set(GLib.get_home_dir());
 
-        return <box onDestroy={() => bd.call()} className="file-chooser bg-widget border border-1 border-secondary rounded-4 p-4">
-            <box vertical={true}>
-                <centerbox hexpand={true}>
-                    <box></box>
-                    <box hexpand={true}>
-                        <entry hexpand={true} text={current_path()} onKeyPressEvent={(self, event) => { 
-                            if (event.get_keycode()[1] == 36 /** enter */) { current_path.set(self.text); files_view.grab_focus(); } }}></entry>
-                    </box>
+        let scroll_bd = new before_delete();
+
+        const scroll = new Widget.Scrollable ({
+            child: files_view,
+            className: "file-chooser-scrollable",
+            expand: true,
+            hscrollbarPolicy: Gtk.PolicyType.NEVER,
+            vscrollbarPolicy: Gtk.PolicyType.AUTOMATIC,
+            onDestroy: () => scroll_bd.call(),
+            sensitive: ready()
+        });
+
+        scroll_bd.add((id: number) => scroll.disconnect(id), scroll.connect ('scroll-event', (self, event: Gdk.Event) => {
+            const ypos = Math.abs(files_view.translate_coordinates(scroll, 0, -scroll.get_allocated_height())[2]); /** y-pos */
+            if (Math.abs(files_view.get_allocated_height() - ypos) <= 78) {
+                grap_next_rows (current_path.get());
+            }
+        }));
+
+        const entry = new Widget.Entry({
+            editable:ready(),
+            hexpand: true,
+            text: current_path(),
+            onKeyPressEvent: (self, event) => { 
+                if (ready.get() && event.get_keycode()[1] == 36 /** enter */) { current_path.set(self.text); files_view.grab_focus(); } }
+        });
+
+        return <box hexpand={true} onDestroy={() => bd.call()} className="file-chooser bg-widget border border-1 border-secondary rounded-4 p-4">
+            <box hexpand={true} vertical={true}>
+                <box vertical={false} hexpand={true}>
                     <box>
+                        <button sensitive={ready()} onClick={() => ready.get() && history.length > 1 && (history.pop()??true) && (current_path.set(history.pop()??current_path.get()))} className="icon-material btn" label="arrow_back_ios_new"></button>
                     </box>
-                </centerbox>
+                    <box hexpand={true} className="px-2">
+                        {entry}
+                    </box>
+                    <box   halign={Gtk.Align.END}>
+                        <button className="icon-material btn" label="arrow_forward_ios" onClick={() => entry.text != current_path.get() && forward_path(entry.text)}></button>
+                    </box>
+                </box>
                 <box className="mt-3">
-                    <scrollable className="file-chooser-scrollable" vexpand={true} hexpand={true} hscrollbarPolicy={Gtk.PolicyType.NEVER} vscrollbarPolicy={Gtk.PolicyType.AUTOMATIC}>
-                        {files_view}
-                    </scrollable>
+                    {scroll}
                 </box>
             </box>
         </box>;
